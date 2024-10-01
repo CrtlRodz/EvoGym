560eae072704354bbd392b950b55825560b04b46
Cari-Anne Moore <carianne.rodz@gmail.com>
2024-10-01 13:09:44 +0200

    Initial commit

---
+++ src/shared/types.ts
@@ -0,0 +1,19 @@
+
+export enum SelectedPage {
+    Home="home",
+    Benefits="benefits",
+    OurClasses="ourclasses",
+    ContactUs="contactus",
+  }
+
+export type BenefitType={
+  icon:JSX.Element,
+  title:string,
+  description:string
+}
+
+export type ClassType={
+  name:string,
+  description?: string,
+  image: string,
+}
